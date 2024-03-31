6d4e30c1d0be4f2a68cade6d9b00e4af72d0b41e
Ievgeniia23 <affari.nuovi.sempre@gmail.com>
2024-04-01 00:33:23 +0200

    Timer part2

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,22 +1,25 @@
 `use strict`
 console.log("timer");
 
-// Описаний в документації
-import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
 
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
-// Додатковий імпорт стилів
 import "izitoast/dist/css/iziToast.min.css";
 
 
+
+const dataDays = document.querySelector("[data-days]");
+const dataHours = document.querySelector("[data-hours]");
+const dataMinutes = document.querySelector("[data-minutes]");
+const dataSeconds = document.querySelector("[data-seconds]");
+
 const selector = document.getElementById("datetime-picker");
-const startBtn = document.querySelector("button")
+const startBtn = document.querySelector("button");
+let userSelectedDate;
+let deltaTime;
 
-
-
-let userSelectedDate // змінна для зберігання обраної користувачем дати.
+startBtn.disabled = true;
 
 flatpickr(selector, {
     enableTime: true,
@@ -24,34 +27,74 @@ flatpickr(selector, {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      userSelectedDate = selectedDates[0]
+      userSelectedDate = selectedDates[0];
       if (userSelectedDate > new Date()) {
-        startBtn.disabled = false
-      } else {
+        startBtn.disabled = false;
+        deltaTime = userSelectedDate - new Date();
+                  
+       } else {
         startBtn.disabled = true;
-        window.alert("Please choose a date in the future")
+        iziToast.show({
+//           // iconUrl: icon,
+          message:"Please choose a date in the future",
+          messageColor: '#ffffff',
+          color: '#ef4040',
+          close: false,
+          position: 'topRight',
+        });
       }
-
-      console.log(userSelectedDate); // отримуємо обрану дату
     },
 })
-
-//Створюємо функцію для запуску лічильника
-function start() {
-
-    const deltaTime = userSelectedDate - new Date();
-
-    console.log("OK");
-    // console.log(userSelectedDate);
-    // console.log(new Date());
-    console.log(deltaTime);
+function startTimer() {
+ const intervalId = setInterval(() => {
+     const time = convertdeltaTime(deltaTime);            
+     updateTimer(time);  
+deltaTime -= 1000;
+if (deltaTime <= 0) {
+  clearInterval(intervalId)
+}                     
+}, 1000);
 }
-startBtn.disabled = true;
 
-startBtn.addEventListener("click", start)
+function convertdeltaTime(deltaTime) {
+  
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+ 
+  const days = pad(Math.floor(deltaTime / day));
+  const hours = pad(Math.floor((deltaTime % day) / hour));
+  const minutes = pad(Math.floor(((deltaTime % day) % hour) / minute)); 
+  const seconds = pad(Math.floor((((deltaTime % day) % hour) % minute) / second));
 
-setInterval(() => {
+  function pad(value) {
+  return String(value).padStart(2, "0");
+}
+  return { days, hours, minutes, seconds };
+ }
 
-}, 1000)
+function updateTimer({ days, hours, minutes, seconds }) {
+  dataDays.textContent = `${days}`;
+  dataHours.textContent = `${hours}`;
+  dataMinutes.textContent = `${minutes}`;
+  dataSeconds.textContent = `${seconds}`;
+}
+
+startBtn.addEventListener("click",  () => {
+    startTimer();
+    startBtn.disabled = true;
+  });
+
+
+
+
+
+
+
+
+
+
+
 
 
