c52b0880b22a6c2628a90d64c598a4e82c18e9ca
Ievgeniia23 <141283292+Ievgeniia23@users.noreply.github.com>
2024-04-04 01:28:11 +0000

    Deploying to gh-pages from @ Ievgeniia23/goit-js-hw-10@ed397f5db459630dc8069590e37cb5e0af81b9fe 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import"./assets/vendor-77e16229.js";console.log("snackbar");
+import"./assets/modulepreload-polyfill-3cfb730f.js";import"./assets/vendor-77e16229.js";console.log("snackbar");const t=document.querySelector('button[type="submit"]');document.querySelector("fieldset");t.addEventListener("click",n);function n(){new Promise((e,o)=>{if(Fulfilld)return e})}
 //# sourceMappingURL=commonHelpers2.js.map
